c3a2caff7304fca92dc1716ba645644839e052b4
SitiMaiSarah980 <32213515+SitiMaiSarah980@users.noreply.github.com>
2018-05-03 10:28:53 +0800

    Mengisi Nilai Attribut

--- src/praktek02mai/RectangleAksi.java
+++ src/praktek02mai/RectangleAksi.java
@@ -2,5 +2,7 @@ package praktek02mai;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar = 3;
     }
 }
